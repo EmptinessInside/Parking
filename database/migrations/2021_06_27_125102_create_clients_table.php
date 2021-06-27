b09d4767527f042022bdde3a679c8fcb510f0646
EmptinessInside <57385336+EmptinessInside@users.noreply.github.com>
2021-06-27 16:18:29 +0300

    Added migrations for base entities

---
+++ database/migrations/2021_06_27_125102_create_clients_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('first_name', 50);
+            $table->string('second_name', 50);
+            $table->string('third_name', 50)->nullable();
+            $table->integer('gender');
+            $table->string('phone', 11)->unique();
+            $table->string('address')->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
