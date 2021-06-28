b3639882f8c9f34c331c4c10bc11a90c2ebe1ac0
EmptinessInside <merkulovvladislav2000@gmail.com>
2021-06-28 17:12:00 +0300

    Bootstrap added and created base template

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,8 +3,17 @@
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 require('./bootstrap');
+
+Vue.use(VueRouter)
+Vue.use(BootstrapVue)
 
 window.Vue = require('vue').default;
 
@@ -19,7 +28,7 @@ window.Vue = require('vue').default;
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('app-component', require('./components/App.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
